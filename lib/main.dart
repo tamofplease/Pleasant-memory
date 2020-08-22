b76c57f5ca45088ac0abe3256e1eb7729b126d4c
tamofplease <riddimdubstep@yahoo.co.jp>
2020-08-23 01:27:52 +0900

    multi_image_pickerを起動

--- lib/main.dart
+++ lib/main.dart
@@ -1,88 +1,84 @@
 import 'package:flutter/material.dart';
-import 'dart:async';
+import 'blocs/blocs.dart';
+import 'package:meple/helper/splash_screen.dart';
+import 'package:meple/views/homes/home_screen.dart';
+import 'package:provider/provider.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:meple/views/auths/auth.dart';
+import 'simple_bloc_delegate.dart';
 
-import 'package:multi_image_picker/multi_image_picker.dart';
-
-void main() => runApp(new MyApp());
-
-class MyApp extends StatefulWidget {
-  @override
-  _MyAppState createState() => new _MyAppState();
+void main() {
+  BlocSupervisor.delegate = SimpleBlocDelegate();
+  WidgetsFlutterBinding.ensureInitialized(); 
+  final authenticationRepository = FireBaseAuthenticationRepository();
+  final userRepo = UserRepository();
+  final placeRepo = PlaceRepository();
+  runApp(
+    MultiBlocProvider(
+      providers: [
+        BlocProvider<AuthenticationBloc>(
+          create: (context) => AuthenticationBloc(authRepository: authenticationRepository)..add(AppStarted()),
+        ),
+        BlocProvider<DrawerBloc> (
+          create: (context) => DrawerBloc(),
+        ),
+        BlocProvider<UserBloc> (
+          create: (context) => UserBloc(userRepo),
+        ),
+        BlocProvider<CategoryBloc> (
+          create: (context) => CategoryBloc(),
+        ),
+        BlocProvider<ImageBloc> (
+          create: (context) => ImageBloc(),
+        ),
+        BlocProvider<PlaceBloc> (
+          create: (context) => PlaceBloc(placeRepo),
+        )
+      ],
+      child: MyApp(),
+    ),
+  );
 }
 
-class _MyAppState extends State<MyApp> {
-  List<Asset> images = List<Asset>();
-  String _error;
-
+class MyApp extends StatelessWidget {
   @override
-  void initState() {
-    super.initState();
-  }
-
-  Widget buildGridView() {
-    if (images != null)
-      return GridView.count(
-        crossAxisCount: 3,
-        children: List.generate(images.length, (index) {
-          Asset asset = images[index];
-          return AssetThumb(
-            asset: asset,
-            width: 300,
-            height: 300,
-          );
-        }),
-      );
-    else
-      return Container(color: Colors.white);
-  }
-
-  Future<void> loadAssets() async {
-    setState(() {
-      images = List<Asset>();
-    });
-
-    List<Asset> resultList;
-    String error;
-
-    try {
-      resultList = await MultiImagePicker.pickImages(
-        maxImages: 300,
-      );
-    } on Exception catch (e) {
-      error = e.toString();
-    }
-
-    // If the widget was removed from the tree while the asynchronous platform
-    // message was in flight, we want to discard the reply rather than calling
-    // setState to update our non-existent appearance.
-    if (!mounted) return;
-
-    setState(() {
-      images = resultList;
-      if (error == null) _error = 'No Error Dectected';
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return new MaterialApp(
-      home: new Scaffold(
-        appBar: new AppBar(
-          title: const Text('Plugin example app'),
-        ),
-        body: Column(
-          children: <Widget>[
-            Center(child: Text('Error: $_error')),
-            RaisedButton(
-              child: Text("Pick images"),
-              onPressed: loadAssets,
+  Widget build(BuildContext context) { 
+    return BlocBuilder<AuthenticationBloc, AuthenticationState>(
+      bloc: BlocProvider.of<AuthenticationBloc>(context),
+      builder: (context, state) { 
+        if(state is AuthenticationInProgress) {
+          return SplashScreen();
+        }
+        if(state is AuthenticationSuccess) {
+          String uid = state.currentUser.uid;
+          return Provider<String>.value (
+            value: uid,
+            child: MaterialApp(
+              title: "Meple",
+              theme: ThemeData(
+                primaryColor: Colors.indigo[900],
+                accentColor: Colors.pink[800],
+                brightness: Brightness.light
+              ),
+              routes: {
+                '/': (context) {
+                  return new Builder(
+                    builder: (BuildContext context) {
+                      return HomeScreen();
+                    }
+                  );
+                },
+              },
             ),
-            Expanded(
-              child: buildGridView(),
-            )
-          ],
-        ),
-      ),
+          );
+        }
+        if(state is AuthenticationFailure) {
+          return AuthPage();
+        }
+        else {
+          return Container();
+        }
+      },
     );
   }
 }
